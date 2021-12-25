8af6f5d50ec74caaaa3682ba9d6faaae2bd6aec4
AKatryukhin <A.Katryukhin@yandex.ru>
2021-12-25 14:05:53 +0300

    created app, added structure

---
+++ src/components/app/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppStyles from './app.module.css';
+
+export const App = () => {
+  return (
+    <div className={ AppStyles.app }>
+     Lets begin
+    </div>
+  );
+}
