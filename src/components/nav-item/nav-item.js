ca4080cf818585524b5c9e83352ef3ba9d2760f2
AKatryukhin <A.Katryukhin@yandex.ru>
2022-01-09 12:37:21 +0300

    style: stylt Nav, NavItem - edit arrow functions

--- src/components/nav-item/nav-item.js
+++ src/components/nav-item/nav-item.js
@@ -1,10 +1,8 @@
 import styles from './nav-item.module.css';
 
-export const NavItem = (props) => {
-  return (
+export const NavItem = props => (
     <div className={`${styles.wrap}`}>
       <span className='mr-2'>{props.children}</span>
       <p className={`text text_type_main-default ${props.styleInactive} mt-0 mb-0`}>{props.text}</p>
     </div>
   );
-};
