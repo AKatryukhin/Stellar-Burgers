19de6932cc652ec0a0ae9914b8e3e999cb9c4f11
AKatryukhin <A.Katryukhin@yandex.ru>
2022-01-12 11:55:42 +0300

    feat: added fetch in useEffect to App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './app.module.css';
 import { AppHeader } from '../app-header/app-header';
-import { data } from '../../utils/data';
+// import { data } from '../../utils/data';
 import { BurgerIngredients } from '../burger-ingredients/burger-ingredients';
 import { BurgerConstructor } from '../burger-constructor/burger-constructor';
+import { INGREDIENTS_URL } from '../../utils/constants';
 
 export const App = () => {
-  return(
-  <div className={styles.page}>
+
+  const [data, setData] = useState({
+    ingredients: [],
+    isLoading: false,
+    isError: false,
+  });
+
+  useEffect(() => {
+    (async () => {
+      setData({ ...data, isLoading: true });
+      try {
+        const response = await fetch(INGREDIENTS_URL);
+        const res = await response.json();
+        setData({
+          ...data,
+          ingredients: res.data,
+          isLoading: false,
+        });
+      } catch (err) {
+        setData({ ...data, isError: true });
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  return (
+    <div className={styles.page}>
       <AppHeader />
       <main className={styles.main}>
-        <BurgerIngredients data={data} />
-        <BurgerConstructor data={data} />
+        <BurgerIngredients data={data.ingredients} />
+        <BurgerConstructor data={data.ingredients} />
       </main>
-  </div>
-  )
+    </div>
+  );
 };
