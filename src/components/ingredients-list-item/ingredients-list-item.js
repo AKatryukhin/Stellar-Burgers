9f978b899c366986f8fc451cc0fe5d0f3fece1f7
AKatryukhin <A.Katryukhin@yandex.ru>
2022-01-12 17:18:43 +0300

    feat: edit IngredientsItem to React.memo

--- src/components/ingredients-list-item/ingredients-list-item.js
+++ src/components/ingredients-list-item/ingredients-list-item.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import styles from './ingredients-list-item.module.css';
 import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
 import { itemPropTypes } from '../../utils/types';
 
-export const IngredientsItem = ({ item, onModalOpen }) => {
+export const IngredientsItem = React.memo(({ item, onModalOpen }) => {
+
+  const handleClick = () => onModalOpen(item);
+
   return (
-    <article onClick={onModalOpen} className={styles.card}>
+    <article onClick={handleClick} className={styles.card}>
       <Counter count={1} size="default" />
       <img src={item.image} alt={item.name} className='mb-1'></img>
       <div className={`${styles.priceWrap} mb-1`}>
@@ -14,7 +18,7 @@ export const IngredientsItem = ({ item, onModalOpen }) => {
       <p className={`${styles.name} text="true" text_type_main-small`}>{item.name}</p>
     </article> 
   )
-};
+});
 
 IngredientsItem.propTypes = {
   item: itemPropTypes.isRequired
