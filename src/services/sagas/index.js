c01a442b7601e951fbef4551d79bb15b9a8195cd
AKatryukhin <A.Katryukhin@yandex.ru>
2022-02-01 15:09:52 +0300

    style: deleted unused saga currentIngredient from project

--- src/services/sagas/index.js
+++ src/services/sagas/index.js
@@ -1,28 +1,21 @@
-import { spawn, call, put, all, takeEvery} from 'redux-saga/effects';
-import {watchLoadIngredients} from "./ingredients";
-import {watchGetOrderNumber} from "./order";
-// import {watchCurrentIngredients} from "./currentIngredient";
-
+import { spawn, call, all } from "redux-saga/effects";
+import { watchLoadIngredients } from "./ingredients";
+import { watchGetOrderNumber } from "./order";
 
 export default function* rootSaga() {
-    const sagas = [
-        watchLoadIngredients,
-        watchGetOrderNumber,
-        // watchCurrentIngredients
-    ];
-    const retrySagas = yield sagas.map(saga => {
-        return spawn(function* () {
-            while (true) {
-                try {
-                    yield call(saga);
-                    break;
-                }
-                catch (e) {
-                    console.log(e);
-                }
-            }
-        });
+  const sagas = [watchLoadIngredients, watchGetOrderNumber];
+  const retrySagas = yield sagas.map((saga) => {
+    return spawn(function* () {
+      while (true) {
+        try {
+          yield call(saga);
+          break;
+        } catch (e) {
+          console.log(e);
+        }
+      }
     });
+  });
 
-    yield all(retrySagas);
+  yield all(retrySagas);
 }
