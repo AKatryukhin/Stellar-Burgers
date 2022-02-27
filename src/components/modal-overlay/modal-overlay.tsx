5b24c8fcd7f0798b7ca21d3dbd8b343951f6c0a6
Alexandr Katryukhin <A.Katryukhin@yandex.ru>
2022-02-27 17:31:17 +0300

    refactor: edit Modal, ModalOverlay

---
+++ src/components/modal-overlay/modal-overlay.tsx
@@ -0,0 +1,21 @@
+import React, { FC } from "react";
+import style from './modal-overlay.module.css';
+import { ModalOverlayProps } from "./modal-overlay.props";
+
+const ModalOverlay: FC<ModalOverlayProps> = ( {onClose}) => {
+ //функция закрытия модального окна по оверлей
+  const handleOverlay = (e: React.SyntheticEvent<EventTarget>) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+ };
+
+  return (
+    (
+      <div onClick={handleOverlay} className={style.overlay}/>
+    )
+  )
+};
+
+
+export default ModalOverlay;
