eb58207054bd21b1a08f5211da9768fe22ce5c34
Alexandr Katryukhin <A.Katryukhin@yandex.ru>
2022-03-29 19:18:09 +0300

    feat: added ws.test

--- src/services/reducers/ws.test.js
+++ src/services/reducers/ws.test.js
@@ -7,68 +7,61 @@ import {
   wsGetUserOrders, wsUserConnectionClosed
 } from "../actions/actionsWS";
 
-export const order = {
-  ingredients: [{
-    calories: 643,
-    carbohydrates: 85,
-    fat: 26,
-    image: "https://code.s3.yandex.net/react/code/bun-01.png",
-    image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
-    image_mobile: "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
-    name: "Флюоресцентная булка R2-D3",
-    price: 988,
-    proteins: 44,
-    type: "bun",
-    __v: 0,
-    _id: "60d3b41abdacab0026a733c7",
-  },
-    {
-      calories: 14,
-      carbohydrates: 11,
-      fat: 22,
-      image: "https://code.s3.yandex.net/react/code/sauce-04.png",
-      image_large: "https://code.s3.yandex.net/react/code/sauce-04-large.png",
-      image_mobile: "https://code.s3.yandex.net/react/code/sauce-04-mobile.png",
-      name: "Соус фирменный Space Sauce",
-      price: 80,
-      proteins: 50,
-      type: "sauce",
-      __v: 0,
-      _id: "60d3b41abdacab0026a733cd",
-    }],
-  name: "Флюоресцентный space бургер",
-  number: 12355,
-  status: "done",
-  _id: '615c66437deb54001ba5f58b',
-  createdAt: "2022-03-20T09:30:58.061Z",
-  updatedAt: "2022-03-22T16:23:10.075Z",
-};
+// export const order = {
+//   ingredients: [{
+//     calories: 643,
+//     carbohydrates: 85,
+//     fat: 26,
+//     image: "https://code.s3.yandex.net/react/code/bun-01.png",
+//     image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
+//     image_mobile: "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
+//     name: "Флюоресцентная булка R2-D3",
+//     price: 988,
+//     proteins: 44,
+//     type: "bun",
+//     __v: 0,
+//     _id: "60d3b41abdacab0026a733c7",
+//   },
+//     {
+//       calories: 14,
+//       carbohydrates: 11,
+//       fat: 22,
+//       image: "https://code.s3.yandex.net/react/code/sauce-04.png",
+//       image_large: "https://code.s3.yandex.net/react/code/sauce-04-large.png",
+//       image_mobile: "https://code.s3.yandex.net/react/code/sauce-04-mobile.png",
+//       name: "Соус фирменный Space Sauce",
+//       price: 80,
+//       proteins: 50,
+//       type: "sauce",
+//       __v: 0,
+//       _id: "60d3b41abdacab0026a733cd",
+//     }],
+//   name: "Флюоресцентный space бургер",
+//   number: 12355,
+//   status: "done",
+//   _id: '615c66437deb54001ba5f58b',
+//   createdAt: "2022-03-20T09:30:58.061Z",
+//   updatedAt: "2022-03-22T16:23:10.075Z",
+// };
 
-export const ordersFeed = {
-    ingredients: [
-      "60d3b41abdacab0026a733c9",
-      "60d3b41abdacab0026a733c8",
-      "60d3b41abdacab0026a733c7",
-      "60d3b41abdacab0026a733c7",
-    ],
-  name: "Флюоресцентный space бургер",
-  number: 12355,
+const orders =  {createdAt: "2022-03-29T15:30:11.130Z",
+  ingredients: ["60d3b41abdacab0026a733c6", "60d3b41abdacab0026a733c6", "60d3b41abdacab0026a733cd"],
+  name: "Краторный space бургер",
+  number: 12423,
   status: "done",
-  _id: '615c66437deb54001ba5f58b',
-  createdAt: "2022-03-20T09:30:58.061Z",
-  updatedAt: "2022-03-22T16:23:10.075Z",
-};
+  updatedAt: "2022-03-29T15:30:11.376Z",
+  _id: "6243260325b9a4001b6eb1e1"}
 
 const payloadUser = {
   total: 10,
   totalToday: 5,
-  orders: [order]
+  orders: [orders]
 }
 
 const payload = {
   total: 10,
   totalToday: 5,
-  orders: [ordersFeed]
+  orders: [orders]
 }
 
 
@@ -115,7 +108,7 @@ describe('webSocket reducer', () => {
       ...state,
       total: 10,
       totalToday: 5,
-      orders: action.payload
+      orders: [orders]
     })
   })
   it('should WS_GET_USER_ORDERS', () => {
@@ -123,7 +116,7 @@ describe('webSocket reducer', () => {
     const state = wsReducer(initialState, action);
     expect(state).toEqual({
       ...state,
-      userOrders: [order],
+      userOrders: [orders],
     })
   })
   it('should WS_CONNECTION_USER_SUCCESS', () => {
